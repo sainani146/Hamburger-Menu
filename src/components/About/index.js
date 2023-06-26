3c8fff9fdf4f2083d9ef2ac5e0bcce0a5dea2476
sainani146 <saikrishnani006006@gamail.com>
2023-06-26 15:07:26 +0000

    first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,19 @@
+// Write your code here
+import './index.css'
+
+const About = () => (
+  <div className="about-image-container">
+    <img
+      className="mobile-about-image"
+      src="https://assets.ccbp.in/frontend/react-js/about-sm-img.png"
+      alt="about"
+    />
+    <img
+      className="desktop-about-image"
+      src="https://assets.ccbp.in/frontend/react-js/about-lg-img.png"
+      alt="about"
+    />
+  </div>
+)
+
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,19 @@
+// Write your code here
+import './index.css'
+
+const Home = () => (
+  <div className="home-image-container">
+    <img
+      className="mobile-home-image"
+      src="https://assets.ccbp.in/frontend/react-js/home-sm-img.png"
+      alt="home"
+    />
+    <img
+      className="desktop-home-image"
+      src="https://assets.ccbp.in/frontend/react-js/home-lg-img.png"
+      alt="home"
+    />
+  </div>
+)
+
+export default Home
